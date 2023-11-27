dffd6cf9f1b7a85cc88c15f26f803d225a913203
VirginiaH <virginianyawira50@gmail.com>
2023-11-27 15:26:01 +0300

    //

--- templating.js
+++ templating.js
@@ -2,10 +2,10 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + "index.html");
+    res.sendFile(__dirname + "/index.html");
 });
 app.get('/about',function(req, res) {
-    res.sendFile(__dirname + "about.html");
+    res.sendFile(__dirname + "/about.html");
 });
 app.listen(3002, function() {
     console.log("Listening on port 3002");
