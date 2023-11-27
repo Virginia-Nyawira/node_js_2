31d21ddb7977145c2fbfca90cda839705b85f543
VirginiaH <virginianyawira50@gmail.com>
2023-11-27 13:26:42 +0300

    express routing

---
+++ routing_param.js
@@ -0,0 +1,33 @@
+var express= require('express');
+var app= express();
+var students = {
+    1 : 'Jane',
+    2 : 'Johncena',
+    3 : 'John Kilo',
+    4 : 'John Maits',
+    5 : 'Johnathani'
+};
+
+// students.forEach(function(student) {
+//     app.get('/students/' + student.id, function(req, res) {
+//         res.send('Name: ' + student.name);
+//     });
+// });
+
+app.get('/',function(req,res){
+    res.send('Hello World');
+});
+app.get('/contact',function(req,res){
+    res.send('Contact us');
+});
+app.get('/contact/:id',function(req,res){
+    res.send('Contact at a requested name '+ students[req.params.id]);
+});
+app.get('/about',function(req,res){
+    res.send('About us');
+});
+
+
+app.listen(3000,function(){
+    console.log('Server started on port 3000');
+});
